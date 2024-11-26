4306d7d1fc1fe5d9edb984b8298466ec13562b79
DanFromUkraine <ovsannikovdana91@gmail.com>
2024-11-26 22:03:20 +0200

    Змінив значення для ключа resources у i18next init. Але помилка не йде

--- app/lib/i18/index.ts
+++ app/lib/i18/index.ts
@@ -1,19 +1,28 @@
-import i18next from 'i18next';
+import i18next, { ResourceLanguage } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './messages/en-En.json';
 import ru from './messages/ru-Ru.json';
 import uk from './messages/uk-Uk.json';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { AVAILABLE_LANGUAGES } from '../definitions';
+
+const translations: Record<AVAILABLE_LANGUAGES, ResourceLanguage> = {
+	en: {
+		translation: en
+	},
+	uk: {
+		translation: uk
+	},
+	ru: {
+		translation: ru
+	}
+};
 
 i18next
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
-		resources: {
-			en,
-			ru,
-			uk
-		},
+		resources: translations,
 		lng: 'en',
 		supportedLngs: ['en', 'uk', 'ru']
 	});
