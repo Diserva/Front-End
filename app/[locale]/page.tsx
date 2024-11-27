ae5f7807a1e0c49a4ab10345719316e91dcd56f7
DanFromUkraine <ovsannikovdana91@gmail.com>
2024-11-27 19:49:09 +0200

    Так, я також підключив функціонал для отримання текстів. Але зараз треба буде пошукати types для тих багатьох функцій, що я використовував

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,16 @@
-import Link from "next/link";
+import Link from 'next/link';
+import initTranslations from '../i18n';
 
-export default function page() {
-  return (
-    <div>page <Link href={"/some"}>click</Link></div>
-  )
+export default async function page({
+	params: { locale }
+}: {
+	params: { locale: string };
+}) {
+	const { t } = await initTranslations(locale, ['home']);
+	console.log({ locale, t });
+	return (
+		<div>
+			{t('smth')} <Link href={'/some'}>click</Link>
+		</div>
+	);
 }
