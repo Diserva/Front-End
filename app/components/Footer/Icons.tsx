1a42e4c33b8b5033b14867678164275ae9e64bb0
Kava <ovsannikovdana91@gmail.com>
2025-02-08 20:43:05 +0200

    Тепер іконки футера мают по ховеру колір свій оригінальний

--- app/components/Footer/Icons.tsx
+++ app/components/Footer/Icons.tsx
@@ -5,9 +5,9 @@ import { LINK_ICONS } from '@/app/lib/constants/footer';
 export default function Icons() {
 	return (
 		<section className='grid grid-cols-3 gap-x-8 gap-y-4 h-fit'>
-			{LINK_ICONS.map(({ Icon, href }) => (
+			{LINK_ICONS.map(({ Icon, href, hoverColor }) => (
 				<a key={href} href={href}>
-					<Icon className='w-8 h-8 text-icon hover:text-accent duration-200' />
+					<Icon className={`w-8 h-8 text-icon duration-200 hover:text-footIcon-${hoverColor}`} />
 				</a>
 			))}
 		</section>
